e4d5adf7dd4be82b6ba74abbb6451f250f6f4325
levanny <lomidzelevani21@gmail.com>
2025-07-24 21:06:08 +0400

    created a simple server that recieves messages from a client and logs everything

--- server/server.py
+++ server/server.py
@@ -1,22 +1,80 @@
-import asyncio
-import grpc
+import logging
+import time
 from concurrent import futures
 
-import proto.stream_pb2 as stream_pb2
-import proto.stream_pb2_grpc as stream_pb2_grpc
+import grpc
 
-class StreamService(stream_pb2_grpc.StreamServiceServicer):
-    async def Chat(self, request_iterator, context):
-        async for msg in request_iterator:
-            yield stream_pb2.ChatMessage(msg=f"Echo: {msg.msg}")
+from prometheus_client import start_http_server, Counter, Histogram
 
-async def serve():
-    server = grpc.aio.server()
-    stream_pb2_grpc.add_StreamServiceServicer_to_server(StreamService(), server)
-    server.add_insecure_port("[::]:50051")
-    await server.start()
-    print("Server started on :50051")
-    await server.wait_for_termination()
+import stream_pb2
+import stream_pb2_grpc
 
-if __name__ == "__main__":
-    asyncio.run(serve())
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+MESSAGES_RECEIVED = Counter('messages_received_total', 'Total messages received')
+MESSAGES_SENT = Counter('messages_sent_total', 'Total messages sent')
+CONNECTIONS_OPEN = Counter('connections_open_total', 'Total connections Opened')
+ERRORS = Counter('errors_total', 'Total errors')
+CONNECTION_DURATION = Histogram(
+    "connection_duration_seconds",
+    "Duration of a streaming connection in seconds"
+)
+
+class StreamServiceServicer(stream_pb2_grpc.StreamServiceServicer):
+    def __init__(self):
+        super().__init__()
+
+    def streamMessage (self, request_iterator, context):
+        CONNECTIONS_OPEN.inc()
+        start = time.time()
+        seq = 0
+        try:
+            for msg in request_iterator:
+                MESSAGES_RECEIVED.inc()
+                logging.info(f'Received message: seq={msg.seq_number} payload={msg.payload}')
+                response = stream_pb2.StreamMessage(
+                    timestamp = msg.timestamp,
+                    seq_number = msg.seq_number,
+                    payload=f'Echo: {msg.payload}'
+                )
+                MESSAGES_SENT.inc()
+                logging.info(f"Sending message: seq={response.seq_number} payload={response.payload}")
+                yield response
+        except Exception as e:
+            ERRORS.inc()
+            logging.error(f"Error in streamMessages: {e}")
+            raise
+        finally:
+            CONNECTIONS_OPEN.dec()
+            CONNECTION_DURATION.observe(time.time() - start)
+
+def serve():
+    start_http_server(8000)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    stream_pb2_grpc.add_StreamServiceServicer_to_server(StreamServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    logging.info("Server started on port 50051, metrics on 8000")
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        logging.info("Server Stopping... ")
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
+
+
+
+
+
+
+
+
+
+
+
