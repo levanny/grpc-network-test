e63f2c457a7b54eb0f9fa571502c4df9990c3fc6
levanny <lomidzelevani21@gmail.com>
2025-07-25 12:19:41 +0400

    added a feature to run `client.py` indefinetely for random durations until stopped using `Ctrl + C`

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import logging
+import random
 import time
 import signal
 import threading
@@ -33,11 +34,15 @@ signal.signal(signal.SIGTERM, handle_signal)
 def now_timestamp():
     return datetime.now(timezone.utc).isoformat()
 
-def generate_messages(duration=60):
+global duration
+
+def generate_messages(duration: int):
     """Generate messages every second for the given duration (seconds)."""
     seq = 0
+    end_at = time.time() + duration
+    duration = random.randint(1, 10)
     start_time = time.time()
-    while time.time() - start_time < duration:
+    while not STOP.is_set() and time.time() <end_at:
         seq += 1
         msg = stream_pb2.StreamMessage(
             timestamp=now_timestamp(),  # We can leave None or use current time
@@ -48,10 +53,6 @@ def generate_messages(duration=60):
         MESSAGES_SENT.inc()
         yield msg
         time.sleep(1)
-        for _ in range(10):
-            if STOP.is_set():
-                return
-            time.sleep(0.1)
 
 def run():
     # Start Prometheus metrics on port 8001
@@ -59,13 +60,29 @@ def run():
 
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = stream_pb2_grpc.StreamServiceStub(channel)
+        break_time = 3
+        responses = None
+        stream_start = 30
+        stream_end = 300
         try:
-            responses = stub.streamMessages(generate_messages(duration=30))
-            for response in responses:
+            while not STOP.is_set():
+                duration = random.randint(stream_start, stream_end)
+                logging.info(f"Starting new message stream for {duration} seconds")
+                responses = stub.streamMessages(generate_messages(duration))
+
+                for response in responses:
+                    MESSAGES_RECEIVED.inc()
+                    logging.info(f"Received from server: seq={response.seq_number} payload={response.payload}")
+
                 if STOP.is_set():
                     break
-                MESSAGES_RECEIVED.inc()
-                logging.info(f"Received from server: seq={response.seq_number} payload={response.payload}")
+
+                logging.info(f"Pausing {break_time} seconds before next stream... ")
+                for _ in range(break_time * 10):
+                    if STOP.is_set():
+                        break
+                    time.sleep(0.1)
+
         except grpc.RpcError as e:
             if e.code() != grpc.StatusCode.CANCELLED:
                 ERRORS.inc()
